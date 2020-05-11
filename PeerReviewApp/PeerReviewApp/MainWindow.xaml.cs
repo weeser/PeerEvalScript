aa118ae916641bbfab8f7c644a3d8d8e4009c479
Josh Weese <weeser@ksu.edu>
2020-05-11 18:19:33 -0500

    adding conversion to c#

---
+++ PeerReviewApp/PeerReviewApp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace PeerReviewApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void uxButton_LoadStudents_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void uxButton_LoadSurvey_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if ((bool)open.ShowDialog())
+            {
+                Excel.Application xlApp = new Excel.Application();
+                Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(open.FileName);
+                var sheet = xlWorkbook.Sheets[1];
+                for(int row)
+            }
+        }
+    }
+}
